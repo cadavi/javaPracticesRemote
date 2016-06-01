e1b34964a1a903a1cb30f970609ea9de946dc62a
Carlos Mendez <carlosdavimm@hotmail.com>
2016-06-01 16:57:00 -0400

    msg

--- NumerosPrimos.java
+++ NumerosPrimos.java
@@ -4,9 +4,12 @@ public class NumerosPrimos{
     System.out.print("Enter the number to evaluate: ");
     Scanner keyboard = new Scanner(System.in);
     int number = keyboard.nextInt();
-    for(int i = 2; i < number; i++){
-      if(number%i != 0){
-        System.out.println("The number is primus: "+number);
+    for(int i = 3; i < number; i++){
+       //System.out.println(i + " " + number + " " +number%i);
+       if(number%i == 0){
+          System.out.println("The number is primus: "+number);
+          System.out.println(i + " " + number + " " +number%i);
+          break;
       }
     }
   }
