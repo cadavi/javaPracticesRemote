4f788b70d6e571802bead577f77af623bbe26755
Carlos Mendez <carlosdavimm@hotmail.com>
2016-06-03 14:53:55 -0400

    Convert to binary finish

--- ConvertToBinary.java
+++ ConvertToBinary.java
@@ -3,20 +3,23 @@ public class ConvertToBinary{
   public static void main(String args[]){
     System.out.print("Enter the number to convert: ");
     Scanner keyboard = new Scanner(System.in);
+    boolean flag = true;
     final int DIVISOR = 2;
     int number = keyboard.nextInt();
     int product;
     int remainder;   
     do{
-      product = number/DIVISOR;
-      remainder = number%DIVISOR;
-      number = product;
-      System.out.print("The product: "+number);
-      System.out.println();
-      ///System.out.println("The remainder: "+remainder);
-      if(number == 1 && remainder == 0){
-        break;
-      }
-    }while(true);
+        product = number/DIVISOR;
+        remainder = number%DIVISOR;
+        number = product;
+        System.out.print(+remainder);
+        if((number == 1 && remainder == 0)
+             || (number == 1 && remainder == 1)
+             || (number == 0 && remainder == 0)){
+          remainder = number;
+          System.out.print(remainder);
+          break;
+        }
+        }while(flag == true);
   }
 }
