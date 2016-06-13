a0974b98dbd890576bfa80b7c00a3d2bd238b76c
Carlos Mendez <carlosdavimm@hotmail.com>
2016-06-13 16:19:17 -0400

    triangle of random numbers started

--- triangleOfRandomNumbers.java
+++ triangleOfRandomNumbers.java
@@ -3,9 +3,12 @@
 between 0 and 9). The height of the triangle should be user specified.
  */
 import java.util.*;
-public class triangleOfRandomNumbers{
+public class TriangleOfRandomNumbers{
   public static void main(String args[]){
-    System.out.print(randomNumbers());
+    System.out.print("Enter the height of triangle: ");
+    Scanner keyboard = new Scanner(System.in);
+    int triangleHeight = keyboard.nextInt();
+    drawTriangle(triangleHeight);
   }
   
   public static int randomNumbers(){
@@ -16,6 +19,9 @@ public class triangleOfRandomNumbers{
     return randomNumber;
   }
   
-  public static void drawTriangle(int triangleSize){
+  public static void drawTriangle(int triangleHeight){
+    for(int i = 0; i < triangleHeight; i++){
+      System.out.println(" " + randomNumbers());
+    }
   }
 }
