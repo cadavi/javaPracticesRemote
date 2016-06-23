a33bb41999dcc9b75090a3631995c96cde57e76d
Carlos Mendez <carlosdavimm@hotmail.com>
2016-06-23 16:19:21 -0400

    commited ArrayOfThreeIntegers

--- ArrayOfThreeIntegers.java
+++ ArrayOfThreeIntegers.java
@@ -5,36 +5,40 @@ with the biggest sum. For example, if the input array is {3,6,5,2,6}, then the o
 array should be: {3,6,5} because 3+6+5=14, which is bigger than 6+5+2=13 and
 5+2+6=13. Write a full program that tests the method.*/
 public class ArrayOfThreeIntegers{
-  public static int[] savedPosition;
   public static void main(String[] args){
-    int[] getArray = sumOfArray(new int[]{3,6,5,2,6});
+    int[] getArray = sumOfArray(new int[]{3,6,5,5,7,3});
+    int maxSum = getArray[0];
     for(int i:getArray){
-      System.out.println(i);
-    }//end for
-    System.out.println("---------------------------");
-    for(int i : savedPosition){
+      if(maxSum < i){
+        maxSum = i;
+      }//end if
       System.out.println(i);
     }//end for
   }//end method main
   
   public static int[] sumOfArray(int[] array){
     final int NUMBER_OF_ELEMENTS = array.length;
-    savedPosition = new int[NUMBER_OF_ELEMENTS];
+    int sumArray = array[0]+array[1]+array[2];
     int sum = 0;
-    int[] sumArray = new int[NUMBER_OF_ELEMENTS];
+    int[] positionArray  = new int[NUMBER_OF_ELEMENTS];
     for(int i = 0; i < array.length; i++){
-      sum = array[i];
       int counter = 0;
-      for(int j = i + 1; j < array.length; j++){
-        sum +=  array[j];
+      sum = array[1];
+      for(int j = i + 2; j < array.length; j++){
         counter++;
+        //System.out.println("Sum: " + sum + " " +array[j]);
+        System.out.println("Sum of array: " + sumArray);
+        System.out.println("another sum: " + sum);
+        sum += array[j];
+        if(sumArray < sum){
+          sumArray = array[j] + array[i+1];
+        }//end if
         if(counter == 2){
           break;
         }//end if
       }//end for
-      savedPosition[i] = array[i];
-      sumArray[i] = sum;
+      //sumArray[i] = i;
     }//end for
-    return sumArray;
+    return array;
   }//end method sumOfArray
 }//end class ArrayOfThreeIntegers
