4df89a7d9ce9ce555f89407c6185d91bf640a02a
Carlos Mendez <carlosdavimm@hotmail.com>
2016-06-30 15:38:59 -0400

    Yathzee poo

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,65 +1,33 @@
 import java.util.*;
 public class Yahtzee{
-  static final int NUMBER_OF_DICE = 5;
-  static final int NUM_REROLLS = 2;
+  public static final int NUMBER_REROLLS = 2;
   
   public static void main(String[] args){
-    int[] dice = new int[NUMBER_OF_DICE];
     Scanner keyboard = new Scanner(System.in);
-    rollDice(dice);
-    
-    for(int i = 0; i < NUM_REROLLS; i++){
-      if(isYahtzee(dice)){// if we got Yahtzee
+    DiceCup dc = new DiceCup();
+    System.out.println(dc);
+    for(int i = 0; i < NUMBER_REROLLS; i++){
+      if(dc.isYahtzee()){
         break;
       }//end if
-      System.out.println(diceToString(dice));
-      System.out.print("Wich dice do you want to reroll");
-      rollDice(convert(keyboard.nextLine()));
+      System.out.println("Wich dice do you want to reroll?");
+      dc.rollDice(convert(keyboard.nextLine()));
+      System.out.println(dc);
     }//end for
-  }//end method main
+    if(dc.isYahtzee()){
+      System.out.println("You got Yahtzee!");
+    }else{
+      System.out.println("Sorry, better luck next time");
+    }//end if
+  }//end main
   
-  public static void rollDice(int[] dice){
-    for(int i = 0; i < dice.length; i++){
-      dice[i] = getRandomDieValue();
-    }//end for
-  }//end method rollDice
-  
-  public static int[] rollDice(){
-    int [] d = new int [NUMBER_OF_DICE] ;
-    for (int i = 0; i < d.length; i++) {
-      d [i] = getRandomDieValue();
-    }
-    return d;
-  }//end method rollDice
-  
-  public static int getRandomDieValue(){
-    return (int)(Math.random() * 6 + 1);
-  }//end method getRandomDieValue
-  
-  public static int[] convert(String s){
+  static int[] convert(String s){
     StringTokenizer st = new StringTokenizer(s);
     int[] a = new int[st.countTokens()];
-    int i = 0; 
+    int i = 0;
     while(st.hasMoreTokens()){
       a[i++] = Integer.parseInt(st.nextToken());
     }//end while
     return a;
-  }//end method convert
-  
-  public static boolean isYahtzee(int[] dice){
-    for(int e1:dice){
-      if(e1 != dice[0]){// evidence that the dice are different
-        return false;
-      }//end if
-    }//end for
-    return true;
-  }//end method isYahtzee
-  
-  public static String diceToString(int[] dice){
-    String result = "Your dice are: ";
-    for(int i = 0; i < NUMBER_OF_DICE; i++){
-      result += dice[i]+" ";
-    }//end for
-    return result;
-  }//end method diceToString
+  }//end emthod convert
 }//end class Yahtzee
