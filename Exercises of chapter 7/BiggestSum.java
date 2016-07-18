2b39d8420ac89cb868e7b7b392630c0c54ce2f46
Carlos Mendez <carlosdavimm@hotmail.com>
2016-07-18 16:34:12 -0400

    Biggest sum efinished

--- Exercises of chapter 7/BiggestSum.java
+++ Exercises of chapter 7/BiggestSum.java
@@ -9,35 +9,36 @@ public class BiggestSum{
     ArrayList<Integer> aL = new ArrayList<Integer>();
     aL.add(05); 
     aL.add(10); 
-    aL.add(20);//5 + 10 + 20 = 35
-    aL.add(30);
-    aL.add(40);
-    aL.add(50);
-    aL.add(60); 
-    aL.add(70);
-    aL.add(80); 
-    aL.add(90);  
-    biggestSum(aL);
-    /*ArrayList<Integer> lastArrayList = biggestSum(aL);
-    for(Integer i:lastArrayList){
-      System.out.println(i);
-    }*///end for
+    aL.add(15);//05 + 10 + 15 = 30
+    aL.add(20);//10 + 15 + 20 = 45
+    aL.add(25);//15 + 20 + 25 = 60
+    aL.add(30);//20 + 25 + 30 = 75
+    aL.add(35);//25 + 30 + 35 = 90
+    aL.add(40);//30 + 35 + 40 = 105
+    showBiggestSum(aL);
   }//end main method 
   
-  public static ArrayList<Integer> biggestSum(ArrayList<Integer> arrayList){
+  private static ArrayList<Integer> totalizedAL(ArrayList<Integer> arrayList){
     ArrayList<Integer> bigSum = new ArrayList<Integer>();
-    Integer sumTotal = 0;
-    int j = 0;
-    for(int i = 0; i < arrayList.size()-1; i++){
-      sumTotal += arrayList.get(i);
-     if(j == 3){
-        bigSum.add(sumTotal);
-        j = 0;
-        sumTotal = 0;
-      }//end if
-      j++;
-     System.out.println(sumTotal);
+    int flag = 0;
+    for(int i = 0; i < arrayList.size()- 2; i++){
+      int sum = arrayList.get(i);
+      for(int j = 1; j < 3; j++){
+        sum += arrayList.get(j+i);
+      }//end for
+      bigSum.add(sum);
     }//end for
     return bigSum;
   }//end method biggestSum
+  
+  public static void showBiggestSum(ArrayList<Integer> arrayList){
+    ArrayList<Integer> aL = totalizedAL(arrayList);
+    int biggestSum = aL.get(0);
+    for(int i = 1; i < aL.size(); i++){
+      if(biggestSum < aL.get(i)){
+        biggestSum = aL.get(i);
+      }//end if
+    }//end for
+    System.out.print("Biggest sum: " + biggestSum);
+  }//end method showBiggestSum
 }//end class BiggestSum
